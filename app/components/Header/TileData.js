f3bfc17159262ca67450f351d91b0ca716be2ff7
hitisinha <hiti.sinha@metrosystems.net>
2018-09-18 16:38:30 +0530

    Locate Us Page

--- app/components/Header/TileData.js
+++ app/components/Header/TileData.js
@@ -8,44 +8,55 @@ import StoreIcon from '@material-ui/icons/Store';
 import WorkIcon from '@material-ui/icons/Work';
 import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
 import { Link } from 'react-router-dom';
+import Tooltip from '@material-ui/core/Tooltip';
 
 export const mailFolderListItems = (
     
   <div>
-    <ListItem button>
-      <ListItemIcon>
-      <Link to="/"><HomeIcon className="drawer-icon" style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
-      </ListItemIcon>
-      <ListItemText style={{fontSize: '15px'}} primary="Home" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-      <Link to="/products"><StoreIcon className="drawer-icon" style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
-      </ListItemIcon>
-      <ListItemText style={{fontSize: '15px'}} primary="Products" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-      <Link to="/services"><WorkIcon className="drawer-icon" style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
-      </ListItemIcon>
-      <ListItemText style={{fontSize: '15px'}} primary="Services" />
-    </ListItem>   
+    <Tooltip title="Home">
+      <ListItem button>
+        <ListItemIcon>
+        <Link to="/"><HomeIcon className="drawer-icon" style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
+        </ListItemIcon>
+        <ListItemText style={{fontSize: '15px'}} primary="Home" />
+      </ListItem>
+    </Tooltip>
+    <Tooltip title="Products">
+      <ListItem button>
+        <ListItemIcon>
+        <Link to="/products"><StoreIcon className="drawer-icon" style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
+        </ListItemIcon>
+        <ListItemText style={{fontSize: '15px'}} primary="Products" />
+      </ListItem>    
+    </Tooltip>
+    <Tooltip title="Services">
+      <ListItem button>
+        <ListItemIcon>
+        <Link to="/services"><WorkIcon className="drawer-icon" style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
+        </ListItemIcon>
+        <ListItemText style={{fontSize: '15px'}} primary="Services" />
+      </ListItem>  
+    </Tooltip>
   </div>  
 );
 
 export const otherMailFolderListItems = (
   <div>
+    <Tooltip title="Locate Us">
      <ListItem button>
-     <ListItemIcon className="drawer-icon">
-     <Link to="/locateus"><PinDropIcon style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
-     </ListItemIcon>
-     <ListItemText style={{fontSize: '15px'}} primary="Locate Us" />
-   </ListItem>
+      <ListItemIcon className="drawer-icon">
+      <Link to="/locateus"><PinDropIcon style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
+      </ListItemIcon>
+      <ListItemText style={{fontSize: '15px'}} primary="Locate Us" />
+     </ListItem>
+   </Tooltip>
+    <Tooltip title="Contact Us">
    <ListItem button>
      <ListItemIcon>
      <Link to="/contactus"><ContactPhoneIcon className="drawer-icon" style={{ fontSize: 36, color:"#1daee4" }} /></Link> 
      </ListItemIcon>
      <ListItemText style={{fontSize: '15px'}}  primary="Contact Us" />
    </ListItem>
+   </Tooltip>
   </div>
 );
--- app/containers/LocateUsPage/CardLocateUs.js
+++ app/containers/LocateUsPage/CardLocateUs.js
@@ -4,10 +4,13 @@ import { withStyles } from '@material-ui/core/styles';
 import classnames from 'classnames';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
+import CardMedia from '@material-ui/core/CardMedia'; 
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import VisitingCard from 'images/visiting_card.jpg';
+import MapCard from 'images/map.jpg';
+import Directions from '@material-ui/icons/Directions';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const styles = theme => ({
   card: {
@@ -36,10 +39,21 @@ class CardLocateUs extends React.Component {
           subheader="Serving You, Curing You!"
         />       
         <CardContent>
-         <Typography className={classes.title} color="textSecondary">
-           You may Locate us either at our number or the email-address.
+        <a href="https://www.google.de/maps/place/Gulati+Pharma/@25.1888526,75.8348495,15z/data=!4m5!3m4!1s0x0:0x75bd7b80836f2d9c!8m2!3d25.1888138!4d75.8419305">
+        <img src={MapCard} alt="Gulati Pharma Location" width='90%'         
+        style={{margin:'auto', maxWidth: '100%',height:'auto', display:'block'}}/>
+        </a>
+         <Typography color="textSecondary">
+         
         </Typography>        
         </CardContent>
+        <CardActions>
+        <Tooltip title="Locate Us">
+          <a href="https://www.google.de/maps/dir//gulati+pharma,1,+Porwal+Medical+Market,+Ladpura,+Kota,+Rajasthan+-+324001/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x396f9b18019b44f3:0x75bd7b80836f2d9c?sa=X&ved=2ahUKEwjnyL3HrcTdAhXE2ywKHRw_CJQQ9RcwC3oECAoQDg">
+            <Directions style={{ fontSize: 36, color:"#1daee4" }} />
+          </a>
+        </Tooltip>
+      </CardActions>
       </Card>
     );
   }
