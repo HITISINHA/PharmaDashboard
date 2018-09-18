468e4f460287ed2b7a81b198a5700a8d5825f3c5
hitisinha <hiti.sinha@metrosystems.net>
2018-09-18 14:37:23 +0530

    Contact Us Card completed

--- app/containers/LocateUsPage/LocateUsPage.js
+++ app/containers/LocateUsPage/LocateUsPage.js
@@ -5,6 +5,7 @@
  */
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import CardLocateUs from './CardLocateUs';
 import './style.scss';
 
 export default class LocateUsPage extends React.Component {
@@ -23,44 +24,10 @@ export default class LocateUsPage extends React.Component {
           <title>Locate Us Page</title>
           <meta
             name="description"
-            content="Locate Us Page of React.js Boilerplate application"
+            content="Locate Us Page of Gulati Pharma application"
           />
         </Helmet>
-        <h1>Features</h1>
-        <ul>
-          <li>
-            <p className="title">Next generation JavaScript</p>
-            <p>
-              Use template strings, object destructuring, arrow functions, JSX
-              syntax and more, today.
-            </p>
-          </li>
-          <li>
-            <p className="title">Instant feedback</p>
-            <p>
-              Enjoy the best DX and code your app at the speed of thought! Your
-              saved changes to the CSS and JS are reflected instantaneously
-              without refreshing the page. Preserve application state even when
-              you update something in the underlying code!
-            </p>
-          </li>
-          <li>
-            <p className="title">Industry-standard routing</p>
-            <p>
-              {
-                "Write composable CSS that's co-located with your components for complete modularity. Unique generated class names keep the specificity low while eliminating style clashes. Ship only the styles that are on the page for the best performance."
-              }
-            </p>
-          </li>
-          <li>
-            <p className="title">The Best Test Setup</p>
-            <p>
-              Automatically guarantee code quality and non-breaking changes.
-              (Seen a react app with 99% test coverage before?)
-            </p>
-          </li>
-        </ul>
-        <i>and much more...</i>
+        <CardLocateUs/>
       </div>
     );
   }
