e39c61ceb45bc79c10d394f1d98359990fad7207
hitisinha <hiti.sinha@metrosystems.net>
2018-09-19 16:54:18 +0530

    -

--- app/containers/ProductsPage/ProductsPage.js
+++ app/containers/ProductsPage/ProductsPage.js
@@ -37,7 +37,7 @@ const companyList =[
 ['Seagull Pharma', Seagull],
 ['Comed', Comed],
 ['BioSearch', BioSearch]
-]
+];
 
 export default class ProductsPage extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
